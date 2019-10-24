7195884722294fd0f371e2257ac8c09ea3c28c6f
Ollie <oliverrarmitage@gmail.com>
2019-10-24 12:57:27 +0100

    CMakeFiles

---
+++ cmake-build-debug/CMakeFiles/Compiler_2_0.dir/DependInfo.cmake
@@ -0,0 +1,29 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/Ollie/CLionProjects/Compiler_2.0/C.tab.c" "/home/Ollie/CLionProjects/Compiler_2.0/cmake-build-debug/CMakeFiles/Compiler_2_0.dir/C.tab.c.o"
+  "/home/Ollie/CLionProjects/Compiler_2.0/arithmetic.c" "/home/Ollie/CLionProjects/Compiler_2.0/cmake-build-debug/CMakeFiles/Compiler_2_0.dir/arithmetic.c.o"
+  "/home/Ollie/CLionProjects/Compiler_2.0/lex.yy.c" "/home/Ollie/CLionProjects/Compiler_2.0/cmake-build-debug/CMakeFiles/Compiler_2_0.dir/lex.yy.c.o"
+  "/home/Ollie/CLionProjects/Compiler_2.0/main.c" "/home/Ollie/CLionProjects/Compiler_2.0/cmake-build-debug/CMakeFiles/Compiler_2_0.dir/main.c.o"
+  "/home/Ollie/CLionProjects/Compiler_2.0/nodes.c" "/home/Ollie/CLionProjects/Compiler_2.0/cmake-build-debug/CMakeFiles/Compiler_2_0.dir/nodes.c.o"
+  "/home/Ollie/CLionProjects/Compiler_2.0/symbol_table.c" "/home/Ollie/CLionProjects/Compiler_2.0/cmake-build-debug/CMakeFiles/Compiler_2_0.dir/symbol_table.c.o"
+  "/home/Ollie/CLionProjects/Compiler_2.0/value.c" "/home/Ollie/CLionProjects/Compiler_2.0/cmake-build-debug/CMakeFiles/Compiler_2_0.dir/value.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../."
+  "../testFiles"
+  "../testFiles/straight-line"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/Compiler_2_0.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Compiler_2_0.dir/C.tab.c.o"
+  "CMakeFiles/Compiler_2_0.dir/arithmetic.c.o"
+  "CMakeFiles/Compiler_2_0.dir/lex.yy.c.o"
+  "CMakeFiles/Compiler_2_0.dir/main.c.o"
+  "CMakeFiles/Compiler_2_0.dir/nodes.c.o"
+  "CMakeFiles/Compiler_2_0.dir/symbol_table.c.o"
+  "CMakeFiles/Compiler_2_0.dir/value.c.o"
+  "Compiler_2_0"
+  "Compiler_2_0.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Compiler_2_0.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
