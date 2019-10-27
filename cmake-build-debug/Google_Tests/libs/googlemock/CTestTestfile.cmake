9422a38948283020e1a07f9750ff5e0dbce8a3c8
Ollie <oliverrarmitage@gmail.com>
2019-10-27 13:12:31 +0000

    Revert "Fix?"
    This reverts commit a7590216c645c4bd7ba8a214a8065efa7b563c23.

--- cmake-build-debug/Google_Tests/libs/googlemock/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ollie/CLionProjects/Compiler_2.0/Google_Tests/libs/googlemock
-# Build directory: /home/ollie/CLionProjects/Compiler_2.0/cmake-build-debug/Google_Tests/libs/googlemock
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("../googletest")
